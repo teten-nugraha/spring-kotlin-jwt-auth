3a327cf2571290ca0f8d3f3c3394b4ff72cf3994
Teten Nugraha <teten.nugraha18@gmail.com>
2022-09-07 17:20:01 +0700

    disable SecurityAutoConfiguration::class

--- src/main/kotlin/id/ten/auth/kotlinjwt/KotlinJwtApplication.kt
+++ src/main/kotlin/id/ten/auth/kotlinjwt/KotlinJwtApplication.kt
@@ -1,9 +1,10 @@
 package id.ten.auth.kotlinjwt
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class KotlinJwtApplication
 
 fun main(args: Array<String>) {
