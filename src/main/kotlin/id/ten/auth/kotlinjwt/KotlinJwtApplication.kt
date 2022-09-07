4a99a679cfd64419e4b4b838bc833ca07b3ccf30
Teten Nugraha <teten.nugraha18@gmail.com>
2022-09-07 16:58:05 +0700

    first launch

---
+++ src/main/kotlin/id/ten/auth/kotlinjwt/KotlinJwtApplication.kt
@@ -0,0 +1,11 @@
+package id.ten.auth.kotlinjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinJwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinJwtApplication>(*args)
+}
---
+++ src/test/kotlin/id/ten/auth/kotlinjwt/KotlinJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.ten.auth.kotlinjwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
