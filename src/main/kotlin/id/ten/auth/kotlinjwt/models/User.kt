6f621fe8abbd6d0a867909e367abfd3d1314d65b
Teten Nugraha <teten.nugraha18@gmail.com>
2022-09-07 17:29:58 +0700

    apply User model

---
+++ src/main/kotlin/id/ten/auth/kotlinjwt/models/User.kt
@@ -0,0 +1,25 @@
+package id.ten.auth.kotlinjwt.models
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Int = 0
+
+    @Column
+    var name = ""
+
+    @Column(unique = true)
+    var email = ""
+
+    @Column
+    var password = ""
+
+}
