87390db9952002477af9c39cfdee0b918863d3bc
Teten Nugraha <teten.nugraha18@gmail.com>
2022-09-08 07:26:16 +0700

    generate jwt

--- src/main/kotlin/id/ten/auth/kotlinjwt/controllers/AuthController.kt
+++ src/main/kotlin/id/ten/auth/kotlinjwt/controllers/AuthController.kt
@@ -5,11 +5,14 @@ import id.ten.auth.kotlinjwt.dtos.Message
 import id.ten.auth.kotlinjwt.dtos.RegisterDto
 import id.ten.auth.kotlinjwt.models.User
 import id.ten.auth.kotlinjwt.service.UserService
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Date
 
 @RestController
 @RequestMapping("/api/auth/")
@@ -36,6 +39,13 @@ class AuthController(private val userService: UserService) {
             return ResponseEntity.badRequest().body(Message("invalid password"))
         }
 
-        return ResponseEntity.ok(user)
+        val issuer = user.id.toString()
+
+        val jwt = Jwts.builder()
+            .setIssuer(issuer)
+            .setExpiration(Date(System.currentTimeMillis() + 60 * 24 * 1000)) // 1 day
+            .signWith(SignatureAlgorithm.HS512, "secret").compact()
+
+        return ResponseEntity.ok(jwt)
     }
 }
